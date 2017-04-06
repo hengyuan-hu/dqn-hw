31abda6721fc3409816549a95a19979649303c73
hengyuan-hu <kevin.huhy@gmail.com>
2017-04-05 23:14:28 -0400

    shift from gym to ale.

--- env.py
+++ env.py
@@ -1,13 +1,13 @@
 """Wrapper of OpenAI Gym enviroment"""
-import gym
-from gym import wrappers
 import cv2
+import os
 from collections import deque
 import numpy as np
-from scipy.misc import imsave
+from ale_python_interface import ALEInterface
+import time
 
 
-SPACE_INVADERS_INIT_LIVES = 3
+glb_counter = 0
 
 
 def preprocess_frame(observ, output_size):
@@ -18,94 +18,125 @@ def preprocess_frame(observ, output_size):
 
 
 class Environment(object):
-    def __init__(self, name, num_frames, frame_size,
-                 record=False, mnt_path=None, video_callable=None,
-                 write_upon_reset=True, negative_dead_reward=False):
-        assert num_frames>0
-        self.env = gym.make(name)
-        self.name = name
-        if record:
-            def capture_all(episode_id):
-                return True
-            def capture_every_twenty(episode_id):
-                return (episode_id % 20) == 0 # return first of each eval
-            if mnt_path is None:
-                mnt_path = './monitor/'
-            if video_callable is None:
-                video_callable = capture_every_twenty
-            # force=True for debug easiness, need care before actual training
-            self.env = wrappers.Monitor(self.env, mnt_path, force=True,
-                                        video_callable=video_callable,
-                                        write_upon_reset=write_upon_reset)
-        self.frame_size = frame_size
+    def __init__(self,
+                 rom_file,
+                 frame_skip,
+                 num_frames,
+                 frame_size,
+                 no_op_start,
+                 rand_seed):
+        self.ale = self._init_ale(rand_seed, frame_skip, rom_file)
+        # normally (160, 210)
+        self.screen_width, self.screen_height = self.ale.getScreenDims()
+        self.actions = self.ale.getMinimalActionSet()
+
+        self.frame_skip = frame_skip
         self.num_frames = num_frames
+        self.frame_size = frame_size
+        self.no_op_start = no_op_start
+
+        self.clipped_reward = 0
+        self.total_reward = 0
+        self.prev_screen = np.zeros(
+            (self.screen_height, self.screen_width, 3), dtype=np.float32)
+        self.frame_queue = deque(maxlen=num_frames)
         self.end = True
-        self.total_reward = 0.0
-        self.frames_queue = deque(maxlen=4)
-        self.negative_dead_reward = negative_dead_reward
 
-    def reset(self):
-        """reset env and frame queue, return initial state """
-        self.end = False
-        self.total_reward = 0.0
-        initial_queue = [np.zeros((self.frame_size, self.frame_size))
-                         for _ in range(self.num_frames-1)]
-        self.frames_queue.extend(initial_queue)
-        initial_state = self.env.reset()
-        initial_state = preprocess_frame(initial_state, self.frame_size)
-        self.frames_queue.append(initial_state)
-
-        if self.negative_dead_reward and self.name == 'SpaceInvaders-v0':
-            self.lives = SPACE_INVADERS_INIT_LIVES
-
-        return np.array(self.frames_queue)
-
-    def render(self):
-        self.env.render()
+    @staticmethod
+    def _init_ale(rand_seed, frame_skip, rom_file):
+        assert os.path.exists(rom_file), '%s does not exists.'
+        ale = ALEInterface()
+        ale.setInt('random_seed', rand_seed)
+        ale.setBool('showinfo', False)
+        ale.setInt('frame_skip', 1)
+        ale.setFloat('repeat_action_probability', 0.0)
+        ale.setBool('color_averaging', False)
+        ale.loadROM(rom_file)
+        return ale
 
     @property
     def num_actions(self):
-        return self.env.action_space.n
+        return len(self.actions)
 
-    def step(self, action):
+    def _get_current_frame(self):
+        # global glb_counter
+        screen = self.ale.getScreenRGB()
+        max_screen = np.maximum(self.prev_screen, screen)
+        frame = preprocess_frame(max_screen, self.frame_size)
+        # cv2.imwrite('test_env/%d.png' % glb_counter, cv2.resize(frame, (800, 800)))
+        # glb_counter += 1
+        return frame
+
+    def reset(self):
+        self.end = False
+        self.clipped_reward = 0
+        self.total_reward = 0
+
+        for _ in range(self.num_frames - 1):
+            self.frame_queue.append(
+                np.zeros((self.frame_size, self.frame_size), dtype=np.float32))
+
+        self.ale.reset_game()
+        n = np.random.randint(0, self.no_op_start)
+        for i in range(n):
+            if i == n - 1:
+                self.prev_screen = self.ale.getScreenRGB()
+            self.ale.act(0)
+
+        self.frame_queue.append(self._get_current_frame())
+
+        return np.array(self.frame_queue)
+
+    def step(self, action_idx):
         """Perform action and return frame sequence and reward.
         Return:
         state: [frames] of length num_frames, 0 if fewer is available
         reward: float
         """
         assert not self.end
-        obs, reward, self.end, info = self.env.step(action)
-        obs = preprocess_frame(obs, self.frame_size)
+        reward = 0
+        clipped_reward = 0
+        for i in range(self.frame_skip):
+            self.prev_screen = self.ale.getScreenRGB()
+            r = self.ale.act(self.actions[action_idx])
+            reward += r
+            clipped_reward += np.sign(r)
 
-        self.frames_queue.append(obs) # left is automatically popped
+            if self.ale.game_over():
+                self.end = True
+                break
+
+        self.frame_queue.append(self._get_current_frame())
         self.total_reward += reward
-
-        # clip reward
-        reward = np.sign(reward)
-
-        # hack reward to be negative if life lost
-        if (self.negative_dead_reward
-            and self.name == 'SpaceInvaders-v0'
-            and info['ale.lives'] < self.lives):
-            reward -= -1.0
-            self.lives = info['ale.lives']
-
-        return np.array(self.frames_queue), reward#, self.end, info
-
-    def close(self):
-        self.env.close()
-
-    def seed(self, seed=None):
-        self.env.seed(seed)
+        self.clipped_reward += clipped_reward
+        return np.array(self.frame_queue), clipped_reward
 
 
 if __name__ == '__main__':
-     env = gym.make('SpaceInvaders-v0')
-     """{
-         0: 'Noop',
-         1: 'Fire',
-         2: 'Right',
-         3: 'Left',
-         4: 'RightFire',
-         5: 'LeftFire'
-     }"""
+    env = Environment('roms/space_invaders.bin', 4, 4, 84, 30, 33)
+
+    state = env.reset()
+    i = 0
+    while not env.end:
+        action = np.random.randint(0, env.num_actions)
+        state, reward = env.step(action)
+        # print state.shape
+        # print state.max(), state.min()
+        i += 1
+    print 'total_reward:', env.total_reward
+    print 'clipped_reward', env.clipped_reward
+    print 'total steps:', i
+
+    state = env.reset()
+    i = 0
+    while not env.end:
+        action = np.random.randint(0, env.num_actions)
+        state, reward = env.step(action)
+        if i == 100:
+            for idx, f in enumerate(state):
+                cv2.imwrite('test_env/f%d.png' % idx,
+                            cv2.resize(f, (800, 800)))
+        i += 1
+    print 'total_reward:', env.total_reward
+    print 'clipped_reward', env.clipped_reward
+    print 'total steps:', i
